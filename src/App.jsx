5e561038d768cad1610b91deecebcdc5a5aff095
rohit kumar <rohitkumarprajapat087@gmail.com>
2025-08-02 15:44:45 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import axios from 'axios'
+import { useEffect } from 'react'
+import './App.css'
+
+function App() {
+  const [data,setData] = useState([])
+
+  const fetchDetails = async() =>{
+    const Details = await axios.get('https://dummyjson.com/products') 
+    console.log(Details)
+    setData(Details.data.products)
+  }
+  
+  useEffect(()=>{
+    fetchDetails()
+  },[])
+
+  return (
+    <div>
+      <h1>Fetch Data</h1>
+      <div style={{display:"flex",flexWrap:"wrap", gap:"20px"}}>
+        {data && data.map((item)=>(
+        <div style={{border:"2px solid", width:"400px", fontSize:"12px", fontWeight:"700"}}>
+          <img src={item.thumbnail} alt="" style={{height:"200px"}}/>
+          <h3 key={item.id}>{item.title}</h3>
+          <p>{item.description}</p>
+          <p>{item.category}</p>
+          <p>Item Price : {item.price}</p>
+          <p>Discount Price : {item.discountPercentage}</p>
+          <p>Rating : {item.rating}</p>
+          <p>WArranty : {item.warrantyInformation}</p>
+          <p>Reviews : {item.comment}</p>
+          
+        </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default App
